f32d7ff657fe1e13b450c0b4bde4b67938ea02b5
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2014-07-01 22:34:54 +0530

    fixes and streaming api

--- bot.py
+++ bot.py
@@ -1,16 +1,20 @@
-import tweepy , time , os , ConfigParser , pickle , sys , random , json , urllib2
+import time , os , ConfigParser , pickle , sys , random , json , urllib2 , tweepy
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
 
 tweet_stat = dict(mention_sl=None)
 random_list =["gathering up Sri Lanka with the power in twitter",
 	"Ayubowan Sri Lanka",
-	"Worlds best contry Sri Lanka",
+	"Worlds best country Sri Lanka",
 	"Twitter Forever",
 	"Sri Lanka, our motherland"]
 config = ConfigParser.ConfigParser()
 config.read('config')
 w_id = "EXX0001"
 auth = None
-times =1
+global times
+times = 1
 
 def read_stat():
 	fh = open("tstat", "r")
@@ -45,24 +49,6 @@ if os.path.isfile("tstat"):
 else:
 	write_stat()
 
-def retweet():
-	read_stat()
-	try:
-		retweets = api.search("#SriLanka",since_id=tweet_stat["mention_sl"])
-	except:
-		retweets = api.search("#SriLanka")
-	for t in retweets:
-		try:
-			if check_ban(t.text,t.user.screen_name):
-				api.retweet(t.id)
-				print("Retweeted: "+str(t.id))
-			else:
-				print("Ban "+t.user.screen_name )
-		except:
-			print("Retweeting Failed")
-	tweet_stat["mention_sl"]=retweets[-1].id
-	write_stat()
-
 def random_tweet():
 	try:
 		api.update_status(random_list[random.randrange(0,len(random_list)-1)])
@@ -74,21 +60,42 @@ def whe_stat():
 	try:
 		html = urllib2.urlopen("http://query.yahooapis.com/v1/public/yql?q=select%20item%20from%20weather.forecast%20where%20location%3D%22C"+w_id+"%22&format=json")
 		json_d = json.loads(html.read())
-		api.update_status("The Wether in colombo looks "+ json_d["query"]["results"]["channel"]["item"]["condition"]["text"])
+		api.update_status("The Whether in colombo looks "+ json_d["query"]["results"]["channel"]["item"]["condition"]["text"])
 		print("Whether Tweet send")
 	except:
 		print("Whether Tweet Failed");
 
-while True:
-	if times ==6:
-		random_tweet()
-		times =1
-	elif times == 5:
-		whe_stat()
-		retweet()
-	retweet()
-	time.sleep(10)
-	print("Time out")
-	time.sleep(600)
-	print("Time out Over")
-	times = times + 1
+class TweetListner(StreamListener):
+    def on_data(self, data):
+		try:
+			t = json.loads(data)
+			if check_ban(t["text"],t["user"]["screen_name"]):
+				api.retweet(t["id"])
+				print("Retweeted: "+str(t["id"]))
+				for ment in t["entities"]["user_mentions"]:
+					if (ment["id"] == 2357666562):
+						api.create_favorite(t["id"])
+						print("Fevorited "+str(t["id"]))
+			else:
+				print("Ban "+t["user"]["screen_name"])
+		except:
+			print("Retweeting Failed")
+		try:
+			self.tim +=1
+		except:
+			self.tim = 1
+		if self.tim<50:
+			random_tweet()
+		elif self.tim <55:
+			whe_stat()
+			self.tim = 1
+		return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+	TL = TweetListner()
+	tweetStr = Stream(auth,TL)
+	tweetStr.filter(track=["#SriLanka","#lka"])	
