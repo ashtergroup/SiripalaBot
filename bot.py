a364b6dcb8758fb07faad821af1a229d1cbc4f03
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2014-09-27 10:58:21 +0530

    fixes

--- bot.py
+++ bot.py
@@ -1,92 +1,94 @@
-import time , os , ConfigParser , pickle , sys , random , json , urllib2 , tweepy
+import time , os , ConfigParser , pickle , sys , random , json , urllib2 , tweepy, thread
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-tweet_stat = dict(mention_sl=None)
-random_list =["gathering up Sri Lanka with the power in twitter",
-	"Ayubowan Sri Lanka",
-	"Worlds best country Sri Lanka",
-	"Twitter Forever",
-	"Sri Lanka, our motherland"]
-config = ConfigParser.ConfigParser()
-config.read('config')
-w_id = "EXX0001"
-auth = None
-global times
-times = 1
+# LOADING AND PARSING RESOURCES
+try:
+	config = ConfigParser.ConfigParser()
+	config.read('config')
+	textrc = json.loads(open("textrc.json").read())
 
-def read_stat():
-	fh = open("tstat", "r")
-	tweet_stat=pickle.load(fh)
-def write_stat():
-	fh = open("tstat", "w")
-	pickle.dump(tweet_stat,fh)
-def check_ban(t,u):
-	if any(word in u for word in ban_user):
-		return False
-	else:
-		if any(word in t for word in words):
-			return False
-		else:
-			return True 
+except:
+	print("Error loading or pasrsing required files")
+	exit()
 
+# AUTHENTICATING WITH TWITTER
 try:
 	auth = tweepy.OAuthHandler(config.get("Auth","a_key"),
 		config.get("Auth","a_secret"))
 	auth.set_access_token(config.get("Auth","a_token"),
 		config.get("Auth","aa_secret"))
-	words = config.get("Config","ban_words").split(' ')
-	ban_user = config.get("Config","ban_user").split(' ')
+	api = tweepy.API(auth)
 except:
 	print("Auth Fail")
 	exit()
 
-api = tweepy.API(auth)
+# MAIN TWEET STREAM HANDLER
+class MainListner(StreamListener):
+	def __init__(self,api,textrc,config):
+		self.textrc = textrc
+		self.api = api
+		try:
+			self.ban_words = config.get("Config","ban_words").split(' ')
+			self.ban_user = config.get("Config","ban_user").split(' ')
+		except:
+			print("Config Parsing failed")
+			exit()
+		thread.start_new_thread(self.Timed_event_handler,(self.random_tweet,5,))
+		for Ad in self.textrc["Ads"]:
+			thread.start_new_thread(self.Timed_event_handler,(self.AdHandle,Ad[1],Ad[0],))
 
-if os.path.isfile("tstat"):
-	print("Config found")
-else:
-	write_stat()
+	def Timed_event_handler(self,run_f,timeout,addi = None):
+		while True:
+			if addi == None:
+				run_f()
+			else:
+				run_f(addi)
+			time.sleep(3600*timeout)	
 
-def random_tweet():
-	try:
-		api.update_status(random_list[random.randrange(0,len(random_list)-1)])
-		print("Random Tweet Send")
-	except:
-		print("Random Tweet Error")
+	def AdHandle(self,Ad):
+		try:
+			self.api.update_status(Ad)
+			print("Ad Tweet Sent")
+		except:
+			print("Ad Tweet Failed")
 
-def whe_stat():
-	try:
-		html = urllib2.urlopen("http://query.yahooapis.com/v1/public/yql?q=select%20item%20from%20weather.forecast%20where%20location%3D%22C"+w_id+"%22&format=json")
-		json_d = json.loads(html.read())
-		api.update_status("The Whether in colombo looks "+ json_d["query"]["results"]["channel"]["item"]["condition"]["text"])
-		print("Whether Tweet send")
-	except:
-		print("Whether Tweet Failed");
+	def random_tweet(self):
+		try:
+			self.api.update_status(self.textrc["random_list"][random.randrange(0,len(self.textrc["random_list"])-1)])
+			print("Random Tweet Send")
+		except:
+			print("Random Tweet Failed")
+	
+	def check_ban(self,t,u):
+		if any(word in u for word in self.ban_user):
+			return False
+		else:
+			if any(word in t for word in self.ban_words):
+				return False
+			else:
+				return True 
 
-class TweetListner(StreamListener):
-    def on_data(self, data):
+	def on_data(self, data):
 		try:
 			t = json.loads(data)
-			if check_ban(t["text"],t["user"]["screen_name"]):
-				api.retweet(t["id"])
-				print("Retweeted: "+str(t["id"]))
+			if self.check_ban(t["text"],t["user"]["screen_name"]):
+				self.api.retweet(t["id"])
+				print("Retweeted: " + t["user"]["screen_name"])
 				for ment in t["entities"]["user_mentions"]:
 					if (ment["id"] == 2357666562):
-						api.create_favorite(t["id"])
-						print("Fevorited "+str(t["id"]))
+						self.api.create_favorite(t["id"])
+						print("Fevorited " + t["user"]["screen_name"])
 			else:
 				print("Ban "+t["user"]["screen_name"])
 		except:
 			print("Retweeting Failed")
 		return True
 
-    def on_error(self, status):
-        print status
+	def on_error(self, status):
+		print(status)
 
-
-if __name__ == '__main__':
-	TL = TweetListner()
-	tweetStr = Stream(auth,TL)
-	tweetStr.filter(track=["#SriLanka","#lka"])	
+ML = MainListner(api,textrc,config)
+tweetStr = Stream(auth,ML)
+tweetStr.filter(track=["#SriLanka","#lka"])	
